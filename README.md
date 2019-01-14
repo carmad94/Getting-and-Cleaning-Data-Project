ae9250c646572fcb68936101232d959c0dd116bf
carmel <carmel@apollo.com.ph>
2019-01-14 13:28:17 +0800

    *Generate variable means by activity and subject *Edit README.md *Generate codebook of new dataset

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Getting-and-Cleaning-Data-Project
+
+This project is part of a requirement for Getting and Cleaning Data. This is to demonstrate on how to get and clean the datasets.
+The raw dataset used for this project comes from the research paper which recognizes Human Activity through Smartphones sensor (Anguita et.al, 2012).
+
+The source code - "run_analysis.R" - generates a tidy dataset which:
+1. Reads all the raw datasets;
+2. Merges the training and test datasets from the raw dataset;
+3. Filters all the mean and std features from the merged dataset;
+4. Adds descriptive labels / variable names from the filtered dataset;
+5. Computes the mean of all variables by subject and activity; and
+6. Export the tidy dataset as .txt
+
+
+Reference:
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+library(dplyr)
 
 # Read training and test data sets
 y_train <- read.table("UCI_HAR_Dataset/train/y_train.txt", sep=" ")
@@ -20,13 +21,19 @@ names(X_test) <- features$V2
 names(y_test) <- c("activity_num")
 names(subject_test) <- c("subject_num")
 
+names(activity_labels)[1] <- c("activity_num")
+names(activity_labels)[2] <- c("activity_label")
+y_train <- left_join(y_train, activity_labels)
+y_test <- left_join(y_test, activity_labels)
+
+
 # Merge training and test data sets to one data set
 har_data_train <- cbind(X_train, y_train, subject_train)
 har_data_test <- cbind(X_test, y_test, subject_test)
 har_data_set <- rbind(har_data_train, har_data_test)
 
 # Extract mean and stdev for each measurement
-har_mean_std <- har_data_set[grepl("std[[:punct:]]|mean[[:punct:]]|_num", names(har_data_set))]
+har_mean_std <- har_data_set[grepl("std[[:punct:]]|mean[[:punct:]]|_num|_label", names(har_data_set))]
 
 # Add descriptive labels / variable names
 names(har_mean_std)[1:66] <- gsub("[()]","",names(har_mean_std[1:66]))
@@ -34,14 +41,10 @@ names(har_mean_std)[1:66] <- gsub("-","_",names(har_mean_std[1:66]))
 names(har_mean_std)[1:40] <- sub("t","Triaxial_",names(har_mean_std[1:40]))
 names(har_mean_std)[41:66] <- sub("f","Fourier_",names(har_mean_std[41:66]))
 
+
 # tidy data set
-activity_mean <- aggregate(har_mean_std[1:66], by=list(har_mean_std$activity_num), FUN=mean)
-subject_mean <- aggregate(har_mean_std[1:66], by=list(har_mean_std$subject_num), FUN=mean)
-names(subject_mean)[1] <- c("subject")
-names(activity_mean)[1] <- c("activity")
-activity_mean[1] <- activity_labels$V2
+activity_subject_mean <- aggregate(har_mean_std[1:66], by=list(har_mean_std$activity_label, har_mean_std$subject_num), FUN=mean)
+names(activity_subject_mean)[1] <- c("activity")
+names(activity_subject_mean)[2] <- c("subject")
 #export the tidy data set
-write.csv(activity_mean, file = "activity_mean.csv",row.names=FALSE, na="")
-write.csv(subject_mean, file = "subject_mean.csv",row.names=FALSE, na="")
-write.table(activity_mean, file = "activity_mean.txt",row.names=FALSE, na="")
-write.table(subject_mean, file = "subject_mean.txt",row.names=FALSE, na="")
+write.table(activity_subject_mean, file = "activity_subject_mean.txt",row.names=FALSE, na="")
